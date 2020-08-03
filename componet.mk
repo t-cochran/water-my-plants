4638a77fb8fd9d16589c1c685dbd16820da79b0b
t-cochran <cochran@colorado.edu>
2020-08-03 11:47:59 -0600

    updated makefiles

--- componet.mk
+++ componet.mk
@@ -1,12 +1,5 @@
+# 
 # Path to component directories
+#
 COMPONENT_DIRS += $(PROJECT_PATH)/main
 COMPONENT_DIRS += $(PROJECT_PATH)/components
-
-# Path to source code sub-directories
-COMPONENT_SRCDIRS += $(PROJECT_PATH)/components/WIFI
-COMPONENT_SRCDIRS += $(PROJECT_PATH)/components/RELAY
-COMPONENT_SRCDIRS += $(PROJECT_PATH)/components/LED
-COMPONENT_SRCDIRS += $(PROJECT_PATH)/components/SENSORS
-
- # Path to headers
-COMPONENT_EXTRA_INCLUDES += $(PROJECT_PATH)/include
--- main/component.mk
+++ main/component.mk
@@ -1,9 +1,4 @@
-# (Uses default behaviour of compiling all source files in directory, adding 
-# 'include' to include path.)
-#
-# This Makefile can be left empty. By default, it will take the sources in the 
-# src/ directory, compile them and link them into lib(subdirectory_name).a 
-# in the build directory. This behaviour is entirely configurable,
-# please read the ESP-IDF documents if you need to do this.
+# 
+# Path to component headers
 #
 COMPONENT_EXTRA_INCLUDES += $(PROJECT_PATH)/include
